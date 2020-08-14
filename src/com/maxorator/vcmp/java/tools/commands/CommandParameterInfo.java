7b3c60e31f5ac9aa3f7473b4928db8c4605cf9c6
Luckshya <luckshyaverma@gmail.com>
2020-08-14 15:19:11 +0530

    Added Optional annotation for optional parameters and some modifications to parse it properly

--- src/com/maxorator/vcmp/java/tools/commands/CommandParameterInfo.java
+++ src/com/maxorator/vcmp/java/tools/commands/CommandParameterInfo.java
@@ -6,12 +6,14 @@ public class CommandParameterInfo {
     public final boolean mayNotFind;
     public final boolean fuzzySearch;
     public final boolean allMatch;
+    public final boolean optional;
 
-    public CommandParameterInfo(Class<?> klass, boolean mayNotFind, boolean fuzzySearch, boolean allMatch) {
+    public CommandParameterInfo(Class<?> klass, boolean mayNotFind, boolean fuzzySearch, boolean allMatch, boolean optional) {
         this.klass = klass;
         this.mayNotFind = mayNotFind;
         this.fuzzySearch = fuzzySearch;
         this.allMatch = allMatch;
+        this.optional = optional;
     }
 
 }
